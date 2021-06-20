d6bd1df616ed38048df1b2fff174ccab05805ac3
Naichuan Zhang <zhangnaichuan168@gmail.com>
2021-06-20 14:06:33 +0800

    home swiper

---
+++ src/components/common/swiper/Swiper.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="hy-swiper">
+    <div class="swiper" @touchstart="touchStart" @touchmove="touchMove" @touchend="touchEnd">
+      <slot></slot>
+    </div>
+    <slot name="indicator"></slot>
+    <div class="indicator">
+      <slot name="indicator" v-if="showIndicator && slideCount > 1">
+        <div v-for="(item, index) in slideCount" class="indi-item"
+             :class="{active: index === currentIndex - 1}" :key="index"></div>
+      </slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Swiper",
+  props: {
+    interval: {
+      type: Number,
+      default: 3000
+    },
+    animDuration: {
+      type: Number,
+      default: 300
+    },
+    moveRatio: {
+      type: Number,
+      default: 0.25
+    },
+    showIndicator: {
+      type: Boolean,
+      default: true
+    }
+  },
+  data() {
+    return {
+      slideCount: 0,
+      totalWidth: 0,
+      swiperStyle: {},
+      currentIndex: 1,
+      scrolling: false,
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,39 @@
 <template>
-  <h2>首页</h2>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+      <home-swiper :banners="banners"/>
+    </nav-bar>
+  </div>
 </template>
 
 <script>
+import NavBar from "@/components/common/navbar/NavBar";
+import {getHomeMultiData} from "@/network/home";
+import HomeSwiper from "@/views/home/children/HomeSwiper";
+
 export default {
-  name: "Home"
+  name: "Home",
+  components: {HomeSwiper, NavBar},
+  data() {
+    return {
+      banners: [],
+      recommends: [],
+    }
+  },
+  created() {
+    getHomeMultiData().then(res => {
+      console.log(res)
+      this.banners = res.data.banner.list
+      this.recommends = res.data.recommend.list
+    })
+  }
 }
 </script>
 
 <style scoped>
-
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
+}
 </style>
